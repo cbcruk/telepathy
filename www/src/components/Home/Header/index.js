4f68b3686b86ede0b00c6b4ad0e269b0b61a595d
Eunsoo Lee <cbcruk@gmail.com>
2019-06-20 23:02:46 +0900

    :star: feature: add refresh button

--- www/src/components/Home/Header/index.js
+++ www/src/components/Home/Header/index.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Link } from '@reach/router'
+import React, { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import * as actions from '../../../actions'
 import Icon from './Icon'
 import styles from './style.module.css'
 
-const Header = () => (
-  <div className={styles.wrapper}>
-    <h1>
-      <Link to="/" className={styles.link}>
-        <Icon className={styles.icon} />
-      </Link>
-    </h1>
-  </div>
-)
+const Header = () => {
+  const dispatch = useDispatch()
+  const fetchItems = useCallback(() => dispatch(actions.fetchItems()), [
+    dispatch
+  ])
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>
+        <button className={styles.link} onClick={fetchItems}>
+          <Icon className={styles.icon} />
+        </button>
+      </h1>
+    </div>
+  )
+}
 
 export default Header
