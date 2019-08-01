ed3843503e651ada67d791c31cc2d245727d0ead
Eunsoo Lee <cbcruk@gmail.com>
2019-08-01 21:40:04 +0900

    :star: feature: add timer

--- www/src/epics/index.js
+++ www/src/epics/index.js
@@ -1,24 +1,30 @@
-import { of } from 'rxjs'
+import { of, timer } from 'rxjs'
 import { ajax } from 'rxjs/ajax'
-import { switchMap, map, catchError } from 'rxjs/operators'
+import { map, catchError, switchMapTo, pluck } from 'rxjs/operators'
 import { combineEpics, ofType } from 'redux-observable'
 import { setItems } from '../actions'
 import { FETCH_ITEMS, FETCH_ITEMS_REJECTED } from '../actions/types'
 
+const timer$ = timer(0, 6000)
+
+const error$ = error =>
+  of({
+    type: FETCH_ITEMS_REJECTED,
+    payload: error
+  })
+
+const ajax$ = ajax.getJSON('/api/rating').pipe(
+  pluck('items'),
+  map(setItems),
+  catchError(error$)
+)
+
+const ajaxWithTimer$ = timer$.pipe(switchMapTo(ajax$))
+
 const fetchEpic = action$ =>
   action$.pipe(
     ofType(FETCH_ITEMS),
-    switchMap(() =>
-      ajax
-        .getJSON('/api/rating')
-        .pipe(
-          map(response => setItems(response.items)),
-          catchError(error => of({
-            type: FETCH_ITEMS_REJECTED,
-            payload: error,
-          }))
-        )
-    )
+    switchMapTo(ajaxWithTimer$)
   )
 
 const rootEpic = combineEpics(fetchEpic)
